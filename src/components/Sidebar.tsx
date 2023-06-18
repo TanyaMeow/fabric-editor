de5420735080e8674c546d3db931839411757e01
Татьяна Хонякина <hanakinat@gmail.com>
2023-06-18 15:34:07 +0300

    Создан функциональный компонент Sidebar

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {Navigation} from "./Navigation";
+
+export function Sidebar() {
+    return (
+        <div className="sidebar">
+            <Navigation/>
+        </div>
+    )
+}
