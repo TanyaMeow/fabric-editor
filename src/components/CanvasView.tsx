eba4c656e160e2185ee41ff8cf3edddd128f268b
Татьяна Хонякина <hanakinat@gmail.com>
2023-06-18 15:25:52 +0300

    Создан функциональный компонент CanvasView

---
+++ src/components/CanvasView.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function CanvasView() {
+    return(
+        <div className="working_field">
+            <canvas className='canvas_field'>Обновите браузер</canvas>
+        </div>
+    )
+}
