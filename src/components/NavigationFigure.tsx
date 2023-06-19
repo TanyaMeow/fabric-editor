da172e1d27649a811a5e3a09156ffce152bdc654
Татьяна Хонякина <hanakinat@gmail.com>
2023-06-19 15:47:05 +0300

    Добавлено состояние, проверка на булево значение и передача состояния в дочерний компонент

--- src/components/NavigationFigure.tsx
+++ src/components/NavigationFigure.tsx
@@ -1,19 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import {FigureContainer} from "./FigureContainer";
 
 export function NavigationFigure() {
+    const [showMenu, setShowMenu] = useState(false);
+
+    const toggleMenu = () => {
+        if(showMenu) {
+            setShowMenu(false);
+        } else {
+            setShowMenu(true);
+        }
+    }
+
     return(
         <div className="menu_figure">
-            <a href="#" id="figure" className="bar_item">
+            <a href="#" onClick={toggleMenu} id="figure" className="bar_item">
                 SVG фигуры
-                <svg className="open_collection" xmlns="http://www.w3.org/2000/svg" height="20"
+                <svg className="open_collection" style={showMenu ? {transform: "rotate(0)"} : {transform: "rotate(-90deg)"}} xmlns="http://www.w3.org/2000/svg" height="20"
                      viewBox="0 -960 960 960"
                      width="20">
                     <path fill="#f4f4ff" d="M480-345 240-585l43-43 197 198 197-197 43 43-240 239Z"/>
                 </svg>
             </a>
-            <div className="menu_svg">
-                <FigureContainer/>
+            <div className={"menu_svg " + (showMenu ? "active" : "")}>
+                <FigureContainer showMenu ={showMenu}/>
             </div>
         </div>
     )
