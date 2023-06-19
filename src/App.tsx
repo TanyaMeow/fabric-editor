8e6e69c61f238d67d2040f1eaa260e92f47092b0
Татьяна Хонякина <hanakinat@gmail.com>
2023-06-19 15:49:26 +0300

    Добавлена все отображаемые компоненты

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {makeDrawFigureButton} from "./components/DrawFigureButton";
+import {Header} from "./components/Header";
+import {Sidebar} from "./components/Sidebar";
+import {CanvasView} from "./components/CanvasView";
+import {Footer} from "./components/Footer";
 
 function App() {
+  // const DrawAdd = makeDrawFigureButton('add', '<svg hui blya>');
+  // const DrawCheck = makeDrawFigureButton('check', 'dsfsafdasf');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <Header />
+        <div className="main">
+          <Sidebar />
+          <CanvasView />
+        </div>
+        <Footer />
+      </div>
+      {/*<DrawAdd />*/}
     </div>
   );
 }
