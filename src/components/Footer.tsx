f7df8a72e408a1dcf8f81c08f6241e41d9397699
Татьяна Хонякина <hanakinat@gmail.com>
2023-06-19 10:16:21 +0300

    Добавлен компонент кнопки

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import {Button} from "./Button";
 
 export function Footer() {
     return (
         <footer className="footer">
             <div className="story">
-
+                <Button className={'undo'}
+                        text={'Назад'}/>
+                <Button className={'redo'}
+                        text={'Вперед'}/>
             </div>
         </footer>
     )
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import {Button} from "./Button";
 
 export function Header() {
     return (
         <header className="header">
-            <div className="tools"></div>
+            <div className="tools">
+                <Button className={'save'}
+                        text={'Сохранить'}/>
+            </div>
         </header>
     )
 }
