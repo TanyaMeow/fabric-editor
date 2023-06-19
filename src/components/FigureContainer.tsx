84f4550e9a1717a9952127f481db4a78ee1f194f
Татьяна Хонякина <hanakinat@gmail.com>
2023-06-19 15:48:54 +0300

    Добавлена проверка и передаваемое состояние из родительского компонента

--- src/components/FigureContainer.tsx
+++ src/components/FigureContainer.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-export function FigureContainer() {
+export function FigureContainer({ showMenu }: any) {
     return (
-        <div className="svg_container">
+        <div className={"svg_container " + (showMenu ? "active" : "")}>
             <div className="new_svg-figure">
                 <svg xmlns="http://www.w3.org/2000/svg" height="25" viewBox="0 -960 960 960" width="25">
                     <path fill="#f4f4ff" d="M450-200v-250H200v-60h250v-250h60v250h250v60H510v250h-60Z"/>
