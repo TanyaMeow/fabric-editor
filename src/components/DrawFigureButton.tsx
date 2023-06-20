c24cf1a0837480e26d331284f89a671aee9f58cc
Татьяна Хонякина <hanakinat@gmail.com>
2023-06-20 11:39:31 +0300

    Добавлены фигуры для отрисовки

--- src/components/DrawFigureButton.tsx
+++ src/components/DrawFigureButton.tsx
@@ -1,12 +1,38 @@
-import React from "react";
+import React, {ReactSVGElement} from "react";
+import {ReactComponent as Square} from "../icons/square.svg";
+import {ReactComponent as Circle} from "../icons/circle.svg";
+import {ReactComponent as Triangle} from "../icons/triagle.svg";
+import {ReactComponent as Heart} from "../icons/heart.svg";
+import {ReactComponent as Star} from "../icons/star.svg";
+import {ReactComponent as Moon} from "../icons/moon.svg";
+import {ReactComponent as Leaf} from "../icons/leaf.svg";
 
-export function DrawFigureButton(path: string) {
-    return (
-        <div className="svg_figure">
-            <svg xmlns="http://www.w3.org/2000/svg" height="35" viewBox="0 -960 960 960" width="35">
-                <path fill="#f4f4ff"
-                      d={path}></path>
-            </svg>
-        </div>
-    )
+export function makeDrawFigureButton(name: string, data: string) {
+    return function DrawFigureButton() {
+        return (
+            <div className="svg_figure" data-svg={data}>
+                { name === 'square' ? <Square fill="#F4F4FF"
+                                              width="35px"
+                                              height="35px"/> : null }
+                { name === 'circle' ? <Circle fill="#F4F4FF"
+                                              width="35px"
+                                              height="35px"/> : null }
+                { name === 'triangle' ? <Triangle fill="#F4F4FF"
+                                                width="35px"
+                                                height="35px"/> : null }
+                { name === 'heart' ? <Heart fill="#F4F4FF"
+                                            width="35px"
+                                            height="35px"/> : null }
+                { name === 'star' ? <Star fill="#F4F4FF"
+                                          width="35px"
+                                          height="35px"/> : null }
+                { name === 'moon' ? <Moon fill="#F4F4FF"
+                                          width="35px"
+                                          height="35px"/> : null }
+                { name === 'leaf' ? <Leaf fill="#F4F4FF"
+                                          width="35px"
+                                          height="35px"/> : null }
+            </div>
+        )
+    }
 }
--- src/components/FigureContainer.tsx
+++ src/components/FigureContainer.tsx
@@ -1,8 +1,24 @@
 import React from "react";
+import {makeDrawFigureButton} from "./DrawFigureButton";
 
 export function FigureContainer({ showMenu }: any) {
+    const Square = makeDrawFigureButton('square', '<svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48"><path d="M180-120q-24 0-42-18t-18-42v-600q0-24 18-42t42-18h600q24 0 42 18t18 42v600q0 24-18 42t-42 18H180Zm0-60h600v-600H180v600Z"/></svg>');
+    const Circle = makeDrawFigureButton('circle', '');
+    const Triangle = makeDrawFigureButton('triangle', '');
+    const Heart = makeDrawFigureButton('heart', '');
+    const Star = makeDrawFigureButton('star', '');
+    const Moon = makeDrawFigureButton('moon', '');
+    const Leaf = makeDrawFigureButton('leaf', '');
+
     return (
         <div className={"svg_container " + (showMenu ? "active" : "")}>
+            <Square />
+            <Circle />
+            <Triangle />
+            <Heart />
+            <Star />
+            <Moon />
+            <Leaf />
             <div className="new_svg-figure">
                 <svg xmlns="http://www.w3.org/2000/svg" height="25" viewBox="0 -960 960 960" width="25">
                     <path fill="#f4f4ff" d="M450-200v-250H200v-60h250v-250h60v250h250v60H510v250h-60Z"/>
