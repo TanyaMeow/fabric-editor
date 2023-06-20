14f58b74ce6bb66e07eb26eddf8acad2d10f2fee
Татьяна Хонякина <hanakinat@gmail.com>
2023-06-20 11:40:08 +0300

    Удален лишний импортированный ReactSvgElement

--- src/components/DrawFigureButton.tsx
+++ src/components/DrawFigureButton.tsx
@@ -1,4 +1,4 @@
-import React, {ReactSVGElement} from "react";
+import React from "react";
 import {ReactComponent as Square} from "../icons/square.svg";
 import {ReactComponent as Circle} from "../icons/circle.svg";
 import {ReactComponent as Triangle} from "../icons/triagle.svg";
