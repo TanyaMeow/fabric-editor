d6d5767d9f63bc3f970a6c6591fbba3aaa59a72f
Татьяна Хонякина <hanakinat@gmail.com>
2023-06-21 09:16:47 +0300

    Добавлены хуки и создание компонента канвас с помощью useMemo

--- src/components/CanvasView.tsx
+++ src/components/CanvasView.tsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, {useEffect, useMemo, useRef} from "react";
+import {Canvas} from "../core/canvasService";
 
 export function CanvasView() {
+    const canvasRef = useRef(null);
+    useEffect(() => {
+        Canvas.init(canvasRef.current);
+    });
+    const canvasComponent = useMemo(() => <canvas id="canvas" className='canvas_field' ref={canvasRef} ></canvas>, []);
+
     return(
         <div className="working_field">
-            <canvas className='canvas_field'>Обновите браузер</canvas>
+            {canvasComponent}
         </div>
     )
 }
