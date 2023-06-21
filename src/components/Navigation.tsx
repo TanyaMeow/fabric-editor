293999bf7f480a72d34802dcbfc229d7b3503c3d
Татьяна Хонякина <hanakinat@gmail.com>
2023-06-21 15:50:11 +0300

    Добавлены компоненты NavigationImage и NavigationText

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import {NavigationFigure} from "./NavigationFigure";
-import {NavigationItem} from "./NavigationItem";
 import {Canvas} from "../core/canvasService";
+import {NavigationImage} from "./NavigationImage";
+import {NavigationText} from "./NavigationText";
 
 export function Navigation() {
     return (
         <div className="block_bar_item">
-            <NavigationItem onAdd={() => Canvas.addText()}
+            <NavigationText onAdd={() => Canvas.addText()}
                             text={'Текст'}/>
             <NavigationFigure/>
-            <NavigationItem text={'Изображения'}/>
+            <NavigationImage />
         </div>
     )
 }
