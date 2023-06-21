8b729176f8eba494e54119b954b2dba2434c90df
Татьяна Хонякина <hanakinat@gmail.com>
2023-06-21 09:18:06 +0300

    Добвалена функция showPopup для передачи текста-канвас в popup

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import {Button} from "./Button";
+import {popupStore} from "../core/popupStore";
+import {Canvas} from "../core/canvasService";
 
 export function Header() {
     return (
         <header className="header">
             <div className="tools">
-                <Button name={'save'}
+                <Button showPopup={() => popupStore.textSvg = Canvas.getSVG()}
+                        name={'save'}
                         text={'Сохранить'}/>
             </div>
         </header>
